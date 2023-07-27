21a7a39d3a82a6949d35d6f7acc749b7eb410de6
beybladebets <beybladee.bets@gmail.com>
2023-07-27 16:28:29 +0200

    twitch add

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,47 +1,52 @@
 import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
-import React, { useEffect } from "react";
+// Business.jsx
+import React, { useEffect, useState } from 'react';
 
+const Business = () => {
+  const [twitchScriptLoaded, setTwitchScriptLoaded] = useState(false);
 
-const FeatureCard = ({ icon, title, content, index }) => (
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
-    </div>
-    <div className="flex-1 flex flex-col ml-3">
-      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
-        {title}
-      </h4>
-      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
-        {content}
-      </p>
-    </div>
-  </div>
-);
+  useEffect(() => {
+    if (!twitchScriptLoaded) {
+      const script = document.createElement('script');
+      script.src = 'https://player.twitch.tv/js/embed/v1.js';
+      script.async = true;
+      script.onload = () => {
+        setTwitchScriptLoaded(true);
+      };
+      document.body.appendChild(script);
+    }
+  }, [twitchScriptLoaded]);
 
+  useEffect(() => {
+    if (twitchScriptLoaded) {
+      // Load the Twitch player when the component mounts
+      new window.Twitch.Player("twitch-embed", {
+        channel: "beyblade_bet",
+        parent: "streamernews.example.com", // Replace with your actual domain
+        muted: true, // Set this to true if you want the player to be muted initially
+      });
+    }
+  }, [twitchScriptLoaded]);
 
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Unleash the fury <br className="sm:block hidden" /> of Beyblade Live battles!
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Live, Thrilling, Unstoppable!
+        </p>
 
-const Business = () =>  (
-  
-  <section id="features" className={layout.section}>
-    <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>
-        Unleash the fury <br className="sm:block hidden" /> of Beyblade Live battles!
-      </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-      Live, Thrilling, Unstoppable!
-      </p>
-      <iframe
-    src="https://player.twitch.tv/?channel=merbleracebet&parent=streamernews.example.com&muted=true"
-    height="720"
-    width="1280"
-    allowfullscreen>
-</iframe>
-      <Button styles={`mt-10`} />
-    </div>
-    
-  </section>
-);
+        <div id="twitch-embed"></div>
+
+        {/* The Button component (if needed) */}
+        <Button styles={`mt-10`} />
+      </div>
+    </section>
+  );
+};
 
 export default Business;
